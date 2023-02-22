c683cd1a66c813259e557b057dd36a4950e3e5ac
vaso-2001 <109334713+vaso-2001@users.noreply.github.com>
2023-02-22 22:20:39 +0400

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from 'react'
+import './App.css'
 function App() {
+  const apiKey = 'e93bea4ca5a57123fe6a3ec010294922'
+  const [weatherData, setWeatherData] = useState([{}])
+  const [city, setCity] = useState("")
+  const getWeather = (event) =>{
+    if(event.key == "Enter"){
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`).then(
+        response => response.json()
+      ).then(
+        data=>{
+          setWeatherData(data)
+          setCity("")
+        }
+      )
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <input
+       className='input'
+       placeholder='Enter City...'
+       onChange={e=>{setCity(e.target.value)}}
+       value={city}
+       onKeyDown = {getWeather}
+       />
+
+       {typeof weatherData.main === 'undefined'? (
+        <div className='welcome'>
+          <p>Welcome to weather app! Enter in a city to get the weather of.</p>
+        </div>
+       ):(
+        <div className='weather-data'>
+          <p className='city'>{weatherData.name}</p>
+          <p className='temp'>{Math.round(weatherData.main.temp)}F</p>
+          <p className='weather'>{weatherData.weather[0].main}</p>
+        </div>
+       )}
+
+       {weatherData.cod === "404" ? (
+        <p className='error'>City not found.</p>
+       ) : (
+        <>
+        </>
+       )}
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
